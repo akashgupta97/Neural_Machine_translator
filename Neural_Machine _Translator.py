331d4e37f2b8e472e63245a4bb433eeff2ca4002
Akash <akash.darknight@gmail.com>
2017-08-20 14:05:00 +0530

    Main Script

--- Neural_Machine _Translator.py
+++ Neural_Machine _Translator.py
@@ -19,3 +19,16 @@ m = 10000
 dataset, human_vocab, machine_vocab, inv_machine_vocab = load_dataset(m)
 
 dataset[:10]
+
+'''
+[('9 may 1998', '1998-05-09'),
+ ('10.09.70', '1970-09-10'),
+ ('4/28/90', '1990-04-28'),
+ ('thursday january 26 1995', '1995-01-26'),
+ ('monday march 7 1983', '1983-03-07'),
+ ('sunday may 22 1988', '1988-05-22'),
+ ('tuesday july 8 2008', '2008-07-08'),
+ ('08 sep 1999', '1999-09-08'),
+ ('1 jan 1981', '1981-01-01'),
+ ('monday may 22 1995', '1995-05-22')]
+'''
